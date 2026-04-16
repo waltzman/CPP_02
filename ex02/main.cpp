b86389dd6b628cfdbdaf16487cca70b5ce855aa4
rlobun <rrw684@gmail.com>
2026-04-16 12:49:54 +0200

    style: prettify output

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: rlobun <rlobun@student.42madrid.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 13:56:41 by rlobun            #+#    #+#             */
-/*   Updated: 2026/04/15 13:33:23 by rlobun           ###   ########.fr       */
+/*   Updated: 2026/04/16 12:48:43 by rlobun           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,17 @@
 int main (void)
 {
 	Fixed 	a;
-	std::cout << Fixed(5.05f)<< std::endl;
-	std::cout << Fixed(2)<< std::endl;
-	Fixed 	b(Fixed(5.05f) * Fixed(2));
-	std::cout << "Fixed(5.05f)		: " << Fixed(5.05f)<< std::endl;
-	std::cout << "Raw bits of 5.05f	: " <<  Fixed(5.05f).getRawBits() << std::endl;
-	std::cout << "Raw bits of b		: " << b.getRawBits() << std::endl;
-	std::cout << std::endl;
+	Fixed const b(Fixed(5.05f) * Fixed(2));
+	
 	std::cout << "a:	" << a << std::endl;
 	std::cout << "++a:	" << ++a << std::endl;
 	std::cout << "a:	" << a << std::endl;
 	std::cout << "a++:	" << a++ << std::endl;
 	std::cout << "a:	" << a << std::endl;
+
 	std::cout << "b:	" << b << std::endl;
 	std::cout << std::endl;
+	
 	std::cout << "Max(a,b): " << Fixed::max(a, b) << std::endl;
 	std::cout << "Min(a,b): " << Fixed::min(a, b) << std::endl;
 	return (0);
